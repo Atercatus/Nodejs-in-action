1e0790da3011cb827b1f0ca396ad94bdb762772a
AterCatus <myleelee2000@nate.com>
2018-07-24 01:35:51 +0900

    add PUB_SUB

---
+++ PUB_SUB/server.js
@@ -0,0 +1,38 @@
+const events = require('events');
+const net = require('net');
+
+let channel = new events.EventEmitter();
+channel.clients = {};
+channel.subscriptions = {};
+
+channel.on('join', function(id, client) {
+    this.clients[id] = client;
+    this.subscriptions[id] = function(senderId, msg) {
+        if(id != senderId) {
+            this.clients[id].write(msg);
+        }
+    }
+    this.on('broadcast', this.subscriptions[id]);
+
+});
+
+channel.on('leave', function(id) {
+    channel.removeListener('broadcast', this.subscriptions[id]);
+    channel.emit('broadcast', id, id + " has left the chat.\n");
+});
+
+let server = net.createServer(function (client) {
+    let id = client.remoteAddress + ':' + client.remotePort;
+    client.on('connect', function() {
+        chennel.emit('join', id, client);
+    });
+    client.on('data', function(data) {
+        data = data.toString();
+        channel.emit('broadcast', id, data);
+    });
+    client.on('close', function() {
+        channel.emit('leave', id);
+    });
+});
+
+server.listen(8888);
